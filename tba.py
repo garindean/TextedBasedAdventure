d5c85ee3f22c346f748a57a2873199cb9180aa14
garindean <roelofsgamingtv@gmail.com>
2019-09-30 11:14:10 -0400

    got rid of my room mistake

--- tba.py
+++ tba.py
@@ -6,16 +6,6 @@ rungame =True
 
 #lights game
 
-class Room:
-    def __init__(self):
-        self.CanGoDirection = {"forward":False,"backward":False}
-        self.roominv = []
-        self.roomBefore = None
-        self.roomAfter = None
-        self.name = ""
-    def printroominv(self):
-        for i in self.roominv:
-            print(i.item)
 
 
 game = Game()
